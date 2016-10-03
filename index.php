8b97db37bc27f3ad00b560a8b933738d30a34b4e
1307894 <joe@6bprincesstreet.com>
2016-10-03 15:07:48 +0100

    commit hobbits

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <body>
 <p>
     <?php
-    $myname = "Joe";
+   /** $myname = "Joe";
     $myage = 22;
 
     if($myage > 16) {
@@ -25,6 +25,26 @@
         print "Name is" . $myname . "you can buy sausage rolls";
     }
     ?>
+    */
+
+$numberOfHobbits = 2;
+switch ($numberOfHobbits){
+    case 1:
+        echo "1 sad hobbit";
+        break;
+    case 2;
+        echo "2 happy hobbits";
+        break;
+    case 3:
+        echo "3 hobbits are a crowd";
+        break;
+    default:
+        echo "All the hobbits have gone home";
+}
+
+?>
+
+
 </p>
 
 
