34af015f28625b59f1e27e59d620e120a4799563
1307894 <joe@6bprincesstreet.com>
2016-10-10 14:26:32 +0100

    connect to database server

---
+++ connect.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1307894
+ * Date: 10/10/2016
+ * Time: 14:09
+ */
+
+$db = new mysqli(
+    "hostname",
+    "username",
+    "password",
+    "db_name"
+);
+
+if($db>connect_errno){
+    die('Connect failed'[$db>connect_error]);
+}
+
+?>
