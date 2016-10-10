e39fb03a080853e56c5ab2c84c7d1e2bb29840bd
1307894 <joe@6bprincesstreet.com>
2016-10-10 14:08:13 +0100

    db connect

--- DBCONNECT.php
+++ DBCONNECT.php
@@ -6,11 +6,11 @@
  * Time: 13:50
  */
 
-define('DB_SERVER','1307894db');
+define('DB_SERVER','us-cdbr-azure-southcentral-f.cloudapp.net');
 define('DB_USERNAME','b54ded4e477e17');
 define('DB_PASSWORD','fcb16056');
 define('DB_DATABASE','1307894db');
 
-$db = mysqli_connect(DB_SERVER. DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 ?>
