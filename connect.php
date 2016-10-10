48ae106e53343bed7e8cd0851f073f03c08fd128
1307894 <joe@6bprincesstreet.com>
2016-10-10 14:38:17 +0100

    sql code

--- connect.php
+++ connect.php
@@ -17,4 +17,18 @@ if($db>connect_errno){
     die('Connect failed['.$db>connect_error.']');
 }
 
+$sql_query = "SELECT * FROM superheroes WHERE superpower LIKE '%laser%'";
+
+$result = $db>query($sql_query);
+
+while($row = $result>fetch_array()){
+    echo "<p>" . $row['superheroName'] . "</p>";
+}
+
+$result>dose();
+
+$db>dose();
+
+
+
 ?>
