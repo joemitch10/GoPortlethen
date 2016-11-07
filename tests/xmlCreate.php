2713f2bb6331efcdba889fa2e054a8809263affb
1307894 <joe@6bprincesstreet.com>
2016-11-07 14:03:00 +0000

    locations

---
+++ tests/xmlCreate.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1307894
+ * Date: 07/11/2016
+ * Time: 14:00
+ */
+
+
+define('DB_SERVER','us-cdbr-azure-southcentral-f.cloudapp.net');
+define('DB_USERNAME','b54ded4e477e17');
+define('DB_PASSWORD','fcb16056');
+define('DB_DATABASE','1307894db');
+
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+if($db>connect_errno){
+    die('Connect failed['.$db>connect_error.']');
+}
+
+// Start XML file, create parent node
+
+$dom = new DOMDocument("1.0");
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
+
+// Select all the rows in the markers table
+
+$query = "SELECT * FROM markers WHERE 1";
+$result = $db->query($query);
+if (!$result) {
+    die('Nothing in result: ');
+}
+
+header("Content-type: text/xml");
+
+// Iterate through the rows, adding XML nodes for each
+
+while ($row = $result->fetch_array()){
+    // ADD TO XML DOCUMENT NODE
+    $node = $dom->createElement("marker");
+    $newnode = $parnode->appendChild($node);
+    $newnode->setAttribute("name",$row['name']);
+    $newnode->setAttribute("address", $row['address']);
+    $newnode->setAttribute("lat", $row['lat']);
+    $newnode->setAttribute("lng", $row['lng']);
+    $newnode->setAttribute("type", $row['type']);
+}
+
+$result->close();
+$db->close();
+
+
+echo $dom->saveXML();
+
+?>
